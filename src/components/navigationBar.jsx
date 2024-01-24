3896fc0be1e021179907d1edd643f71f97f3dfa8
saif qamar <adlyaqeen2@gmail.com>
2024-01-24 15:58:10 +0500

    fixed the responsivness of nav

--- src/components/navigationBar.jsx
+++ src/components/navigationBar.jsx
@@ -4,22 +4,27 @@ export default function NavigationBar(){
     const [isOpen , setIsOpen] = useState(false)
 
     const openNav = () =>{
-        setIsOpen(!isOpen)
+        setIsOpen(true)
+    }
+
+    const closeNav = () =>{
+        setIsOpen(false)
     }
 
     return(
-        <div className=" absolute w-full right-0 flex justify-center z-50">
-            <div className=" h-screen bg-none fixed">
-                <div className="navigationBar  translate-y-2/4 w-[556px] min-w-[656px]">
-                    <div className="trans-back flex flex-col gap-y-3 p-3 rounded-xl bg-neutral-900 bg-opacity-50">
-                        <div className={isOpen ? "above flex flex-row justify-between bg-aw-color rounded-lg p-4 " : "hidden above flex flex-row justify-between bg-aw-color rounded-lg p-4 "}>
-                            <div className="first-col pl-3 border-l border-dotted border-neutral-600">
-                                <li className="text-white -ml-3.5 mb-6 text-xs list-disc">Awards</li>
-                                <div className="all flex flex-col text-white pl-3">
-                                    <a href="" className="pt-2 pb-2 mb-4 text-sm">Winners</a>
-                                    <a href="" className="pt-2 pb-2 mb-4 text-sm">Nominees</a>
-                                    <a href="" className="pt-2 pb-2 mb-4 text-sm">Sites of day</a>
-                                    <a href="" className="pt-2 pb-2 mb-4 text-sm">Winners</a>
+        <div className="fixed bottom-2 lg:bottom-8 w-full z-50 px-5">
+                <div className="navigationBar mx-auto container w-auto md:w-[556px] md:min-w-[656px]">
+                    <div className={isOpen ? "trans-back w-auto md:w-auto mx-auto flex flex-col gap-y-3 p-3 rounded-xl bg-neutral-900 bg-opacity-50" : "trans-back w-fit md:w-auto mx-auto flex flex-col gap-y-3 p-3 rounded-xl bg-neutral-900 bg-opacity-50"}>
+                        
+                        {/* Options Menu */}
+                        <div className={isOpen ? "above h-[600px] md:h-auto overflow-y-scroll w-full flex flex-col md:flex-row justify-between bg-aw-color rounded-lg p-6 " : "hidden above  flex-row justify-between bg-aw-color rounded-lg p-4 "}>
+                            <div className="first-col border-l border-dotted border-neutral-600">
+                                <li className="text-neutral-400 pl-3 -ml-3.5 mb-6 text-xs list-disc">Awards</li>
+                                <div className="all flex flex-col text-white ">
+                                    <a href="" className="pt-2 pb-2 pl-3 mb-4 text-sm border-neutral-800 hover:border-l hover:transition-all hover:duration-200 hover:border-lime-600">Winners</a>
+                                    <a href="" className="pt-2 pb-2 pl-3 mb-4 text-sm border-neutral-800 hover:border-l hover:transition-all hover:duration-200 hover:border-lime-600">Nominees</a>
+                                    <a href="" className="pt-2 pb-2 pl-3 mb-4 text-sm border-neutral-800 hover:border-l hover:transition-all hover:duration-200 hover:border-lime-600">Sites of day</a>
+                                    <a href="" className="pt-2 pb-2 pl-3 mb-4 text-sm border-neutral-800 hover:border-l hover:transition-all hover:duration-200 hover:border-lime-600">Winners</a>
                                 </div>
                             </div>
 
@@ -33,7 +38,7 @@ export default function NavigationBar(){
                                 </div>
 
                                 <div className="Academy pl-3 border-l border-dotted border-neutral-600">
-                                    <li className="text-white -ml-3.5 mb-6 text-xs list-disc">Academy</li>
+                                    <li className="text-neutral-400 -ml-3.5 mb-6 text-xs list-disc">Academy</li>
                                     <div className="all flex flex-col text-white pl-3">
                                         <a href="" className="pt-2 pb-2 mb-4 text-sm">Winners</a>
                                         <a href="" className="pt-2 pb-2 mb-4 text-sm">Nominees</a>
@@ -53,7 +58,7 @@ export default function NavigationBar(){
                                 </div>
 
                                 <div className="Academy pl-3 border-l border-dotted border-neutral-600">
-                                    <li className="text-white -ml-3.5 mb-6 text-xs list-disc">Academy</li>
+                                    <li className="text-neutral-400 -ml-3.5 mb-6 text-xs list-disc">Academy</li>
                                     <div className="all flex flex-col text-white pl-3">
                                         <a href="" className="pt-2 pb-2 mb-4 text-sm">Winners</a>
                                         <a href="" className="pt-2 pb-2 mb-4 text-sm">Nominees</a>
@@ -73,7 +78,7 @@ export default function NavigationBar(){
                                 </div>
 
                                 <div className="Academy pl-3 border-l border-dotted border-neutral-600">
-                                    <li className="text-white -ml-3.5 mb-6 text-xs list-disc">Academy</li>
+                                    <li className="text-neutral-400 -ml-3.5 mb-6 text-xs list-disc">Academy</li>
                                     <div className="all flex flex-col text-white pl-3">
                                         <a href="" className="pt-2 pb-2 mb-4 text-sm">Winners</a>
                                         <a href="" className="pt-2 pb-2 mb-4 text-sm">Nominees</a>
@@ -84,17 +89,17 @@ export default function NavigationBar(){
                             </div>
                         </div>
 
-
-                        <div className="below bg-aw-color  rounded-lg flex flex-row justify-between h-[60px]">
-                            <div className="left rounded-lg w-full justify-between px-5 flex flex-row items-center gap-x-2 text-white"> 
-                                <div className="left gap-x-4 flex justify-between" onClick={openNav}>
-                                    <p>w.home</p>
-                                    <p>---</p>
+                        {/* Main Nav Bar */}
+                        <div className="below rounded-lg  w-auto flex flex-row gap-x-3 justify-between h-[60px]">
+                            <div className={isOpen ? "left bg-aw-color rounded-lg w-full md:w-full justify-around px-5 flex flex-row items-center gap-x-2 text-white"  : "left bg-aw-color rounded-lg w-[150px] md:w-full justify-around px-5 flex flex-row items-center gap-x-2 text-white" }> 
+                                <div className="left gap-x-2 w-full flex items-center w-12/12 justify-between">
+                                    <p className="font-semibold">w.home</p>
+                                    <img className={isOpen ? 'hidden' : 'h-5 w-auto block cursor-pointer'} src="https://i.pinimg.com/originals/26/9d/d1/269dd16fa1f5ff51accd09e7e1602267.png" onClick={openNav} />
                                 </div>
-                                <div className={isOpen ? "close text-white" : "close text-white hidden"}>x</div>
+                                <div className={isOpen ? "close text-white cursor-pointer" : "close text-white hidden"} onClick={closeNav}>x</div>
                             </div>
                             
-                            <div className={isOpen ? " hidden right flex justify-between gap-x-2 rounded-lg bg-neutral-700 p-2" : "right flex justify-between gap-x-2 rounded-lg w-9/12 bg-neutral-700 p-2"}>
+                            <div className={isOpen ? " links hidden right justify-between gap-x-2 rounded-lg bg-neutral-700 p-2" : "right md:flex justify-between gap-x-2 hidden md:w-10/12 rounded-lg bg-neutral-700 p-2"}>
                                 <a href="">
                                     <p className="px-2 text-base font-semibold flex items-center border border-neutral-500 rounded-lg h-full text-neutral-400">Home</p>
                                 </a>
@@ -120,7 +125,7 @@ export default function NavigationBar(){
                     </div>
                 </div>
                 
-            </div>
+            
         </div>
         
     )
